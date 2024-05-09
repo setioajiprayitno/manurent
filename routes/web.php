7ead9206c52de70c40120aef95fe4c6fc13de403
FajarRiansyah <fajary781@gmail.com>
2024-05-09 18:09:28 +0700

    fix value of jenisMobil and transmisi not catched by controller

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,5 @@ Route::get('/car', function () {
 Route::get('/checkout', function () {
     return view('checkout');
 });
+
 Route::post('/send', [BookingController::class, 'send'])->name('booking.send');
-
-
-// Route::get('/test', [BookingController::class, 'index']);
