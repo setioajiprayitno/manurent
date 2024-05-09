af89e815fb64d627288ec258cd16affce548b321
setioajiprayitno <setioajiprayitno11@gmail.com>
2024-05-09 19:29:43 +0700

    hampir final

--- resources/views/emails/booking.blade.php
+++ resources/views/emails/booking.blade.php
@@ -1,20 +1,20 @@
 <x-mail::message>
     # BOOKING
 
-    nama : {{ $data['name']}}
-    nama : {{ $data['phoneNumber']}}
-    nama : {{ $data['address']}}
-    nama : {{ $data['email']}}
-    nama : {{ $data['jenisMobil']}}
-    nama : {{ $data['transmisi']}}
-    nama : {{ $data['time']}}
-    nama : {{ $data['date']}}
-    nama : {{ $data['driver']}}
+    Nama        : {{ $data['name']}}
+    Nomor HP    : {{ $data['phoneNumber']}}
+    Alamat      : {{ $data['address']}}
+    Email       : {{ $data['email']}}
+    Jenis Mobil : {{ $data['jenisMobil']}}
+    Transmisi   : {{ $data['transmisi']}}
+    Jam         : {{ $data['time']}}
+    Tanggal     : {{ $data['date']}}
+    Supir       : {{ $data['driver']}}
 
-    <x-mail::button :url="''">
+    <!-- <x-mail::button :url="''">
         Button Text
     </x-mail::button>
 
-    Thanks,<br>
+    Thanks,<br> -->
     {{ config('app.name') }}
 </x-mail::message>
